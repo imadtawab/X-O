677fca2dd507251d218218952ad649cb7db28d5b
imadtawab <89462211+imadtawab@users.noreply.github.com>
2021-12-20 23:08:50 +0100

    Add files via upload

--- project6.js
+++ project6.js
@@ -1,39 +1,204 @@
 const caro = document.querySelectorAll(".container button");
-const winner1 = document.querySelector(".winner1");
-const winner2 = document.querySelector(".winner2");
-const winner3 = document.querySelector(".winner3");
-const winner4 = document.querySelector(".winner4");
-const winner5 = document.querySelector(".winner5");
-const winner6 = document.querySelector(".winner6");
-const winner7 = document.querySelector(".winner7");
-const winner8 = document.querySelector(".winner8");
-const winner9 = document.querySelector(".winner9");
-// caro.addEventListener("click", (eo) => {
-//     eo.target.classList.toggle("ix")
-//     eo.target.classList.toggle("io")
-// })
+const winner = document.querySelector(".winner");
+const shadow = document.querySelector(".shadow");
+const restar = document.querySelector(".shadow button");
+const player1 = document.querySelector(".player1 span");
+const player2 = document.querySelector(".player2 span");
+let p1 = 0
+let p2 = 0
 
 caro.forEach((item) => {
   item.addEventListener("click", (eo) => {
     item.setAttribute("disabled", "");
-    // item.classList.add("check")
 
-    const xo = document.createElement("span");
     if (item.parentElement.classList.contains("ix")) {
-      xo.innerText = "+";
-      xo.classList.add("x");
+      item.classList.add("x");
+      player2.parentElement.classList.add("active")
+      player1.parentElement.classList.remove("active")
     } else if (item.parentElement.classList.contains("io")) {
-      xo.innerText = "0";
-      xo.classList.add("o");
+      item.classList.add("o");
+      player2.parentElement.classList.remove("active")
+      player1.parentElement.classList.add("active")
     }
-
-    eo.target.prepend(xo);
     item.parentElement.classList.toggle("ix");
     item.parentElement.classList.toggle("io");
-    //     if (caro[0].firstChild.classList.contains("x") ) {
-    //         winner1.classList.add("w123")
-    //         console.log("object");
-    //     }
-    
+    if (
+      (caro[0].classList.contains("x") &&
+        caro[1].classList.contains("x") &&
+        caro[2].classList.contains("x")) ||
+      (caro[0].classList.contains("o") &&
+        caro[1].classList.contains("o") &&
+        caro[2].classList.contains("o"))
+    ) {
+      winner.classList.add("w012");
+    } else if (
+      (caro[3].classList.contains("x") &&
+        caro[4].classList.contains("x") &&
+        caro[5].classList.contains("x")) ||
+      (caro[3].classList.contains("o") &&
+        caro[4].classList.contains("o") &&
+        caro[5].classList.contains("o"))
+    ) {
+      winner.classList.add("w345");
+
+    } else if (
+      (caro[6].classList.contains("x") &&
+        caro[7].classList.contains("x") &&
+        caro[8].classList.contains("x")) ||
+      (caro[6].classList.contains("o") &&
+        caro[7].classList.contains("o") &&
+        caro[8].classList.contains("o"))
+    ) {
+      winner.classList.add("w678");
+    } else if (
+      (caro[0].classList.contains("x") &&
+        caro[3].classList.contains("x") &&
+        caro[6].classList.contains("x")) ||
+      (caro[0].classList.contains("o") &&
+        caro[3].classList.contains("o") &&
+        caro[6].classList.contains("o"))
+    ) {
+      winner.classList.add("w036");
+    } else if (
+      (caro[1].classList.contains("x") &&
+        caro[4].classList.contains("x") &&
+        caro[7].classList.contains("x")) ||
+      (caro[1].classList.contains("o") &&
+        caro[4].classList.contains("o") &&
+        caro[7].classList.contains("o"))
+    ) {
+      winner.classList.add("w147");
+    } else if (
+      (caro[2].classList.contains("x") &&
+        caro[5].classList.contains("x") &&
+        caro[8].classList.contains("x")) ||
+      (caro[2].classList.contains("o") &&
+        caro[5].classList.contains("o") &&
+        caro[8].classList.contains("o"))
+    ) {
+      winner.classList.add("w258");
+    } else if (
+      (caro[0].classList.contains("x") &&
+        caro[4].classList.contains("x") &&
+        caro[8].classList.contains("x")) ||
+      (caro[0].classList.contains("o") &&
+        caro[4].classList.contains("o") &&
+        caro[8].classList.contains("o"))
+    ) {
+      winner.classList.add("w048");
+    } else if (
+      (caro[2].classList.contains("x") &&
+        caro[4].classList.contains("x") &&
+        caro[6].classList.contains("x")) ||
+      (caro[2].classList.contains("o") &&
+        caro[4].classList.contains("o") &&
+        caro[6].classList.contains("o"))
+    ) {
+      winner.classList.add("w246");
+    }
+    if (
+      caro[0].classList.contains("x") &&
+        caro[1].classList.contains("x") &&
+        caro[2].classList.contains("x") ||
+        
+        caro[3].classList.contains("x") &&
+        caro[4].classList.contains("x") &&
+        caro[5].classList.contains("x") ||
+        
+        caro[6].classList.contains("x") &&
+        caro[7].classList.contains("x") &&
+        caro[8].classList.contains("x") ||
+        
+        caro[0].classList.contains("x") &&
+        caro[3].classList.contains("x") &&
+        caro[6].classList.contains("x") ||
+        
+        caro[1].classList.contains("x") &&
+        caro[4].classList.contains("x") &&
+        caro[7].classList.contains("x") ||
+        
+        caro[2].classList.contains("x") &&
+        caro[5].classList.contains("x") &&
+        caro[8].classList.contains("x") ||
+        
+        caro[0].classList.contains("x") &&
+        caro[4].classList.contains("x") &&
+        caro[8].classList.contains("x") ||
+        
+        caro[2].classList.contains("x") &&
+        caro[5].classList.contains("x") &&
+        caro[8].classList.contains("x") 
+    ) {
+      p1++
+      player1.innerText = p1
+      shadow.classList.remove("dn")
+      shadow.querySelector(".winner-x").classList.remove("dn")
+    } else if (
+        caro[0].classList.contains("o") &&
+        caro[1].classList.contains("o") &&
+        caro[2].classList.contains("o") ||
+        
+        caro[3].classList.contains("o") &&
+        caro[4].classList.contains("o") &&
+        caro[5].classList.contains("o") ||
+        
+        caro[6].classList.contains("o") &&
+        caro[7].classList.contains("o") &&
+        caro[8].classList.contains("o") ||
+        
+        caro[0].classList.contains("o") &&
+        caro[3].classList.contains("o") &&
+        caro[6].classList.contains("o") ||
+        
+        caro[1].classList.contains("o") &&
+        caro[4].classList.contains("o") &&
+        caro[7].classList.contains("o") ||
+        
+        caro[2].classList.contains("o") &&
+        caro[5].classList.contains("o") &&
+        caro[8].classList.contains("o") ||
+        
+        caro[0].classList.contains("o") &&
+        caro[4].classList.contains("o") &&
+        caro[8].classList.contains("o") ||
+        
+        caro[2].classList.contains("o") &&
+        caro[5].classList.contains("o") &&
+        caro[8].classList.contains("o") 
+    ) {
+      p2++
+      player2.innerText = p2
+      shadow.classList.remove("dn")
+      shadow.querySelector(".winner-o").classList.remove("dn")
+    } 
+    if (
+      caro[0].className !== "caro" &&
+      caro[1].className !== "caro" &&
+      caro[2].className !== "caro" &&
+      caro[3].className !== "caro" &&
+      caro[4].className !== "caro" &&
+      caro[5].className !== "caro" &&
+      caro[6].className !== "caro" &&
+      caro[7].className !== "caro" &&
+      caro[8].className !== "caro" &&
+      shadow.className == "shadow dn"
+      ) {
+        shadow.classList.remove("dn")
+    }
   });
 });
+
+restar.addEventListener("click", (eo) => {
+      shadow.classList.add("dn")
+      shadow.querySelector(".winner-x").classList.add("dn")
+      shadow.querySelector(".winner-o").classList.add("dn")
+      winner.removeAttribute("class")
+      
+      caro.forEach(item => {
+        item.classList.remove("x")
+        item.classList.remove("o")
+    item.removeAttribute("disabled");
+
+      });
+      winner.classList.add("winner")
+})
